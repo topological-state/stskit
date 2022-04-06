710681103e1233ccadb77032cadfe34d0449007f
matthias muntwiler <matthias@muntwiler.net>
2022-04-06 23:29:27 +0200

    konflikte, folgezuege, farbschema in gleisbelegung

--- gleisbelegung.py
+++ gleisbelegung.py
@@ -31,7 +31,6 @@ def gleisname_sortkey(s: str) -> Tuple[str, int, str]:
         return mo.group(1), mo.group(2), mo.group(3)
 
 
-# todo : farbschema
 # farben = {g: mpl.colors.TABLEAU_COLORS[i % len(mpl.colors.TABLEAU_COLORS)]
 #           for i, g in enumerate(self.client.zuggattungen)}
 # colors = [farben[b[5]] for b in bars]
@@ -47,37 +46,42 @@ farben = [k for k in mpl.colors.TABLEAU_COLORS]
 class Slot:
     zug: ZugDetails
     plan: FahrplanZeile
-    gruppe: str = ""
+    gleis: str = ""
     zeit: int = 0
     dauer: int = 0
-    partner: Optional[ZugDetails] = None
+    kuppelzug: Optional[ZugDetails] = None
     konflikte: List['Slot'] = field(default_factory=list)
 
     def __eq__(self, other):
-        return self.zug.name == other.zug.name and self.gruppe == other.gruppe and self.zeit == other.zeit
+        return self.zug.name == other.zug.name and self.gleis == other.gleis and self.zeit == other.zeit
 
     @property
     def farbe(self) -> str:
-        if self.konflikte:
-            return 'tab:red'
-        elif self.partner:
+        if self.zug.gattung in {'ICE', 'TGV'}:
             return 'tab:orange'
-        elif self.zug.gattung in {'ICE', 'TGV'}:
-            return 'tab:purple'
         elif self.zug.gattung in {'IC', 'EC', 'IR', 'IRE'}:
             return 'tab:green'
         elif self.zug.gattung in {'RE', 'RB'}:
             return 'tab:blue'
-        elif self.zug.gattung.startswith('S'):
-            return 'tab:cyan'
+        elif self.zug.gattung in {'S'}:
+            return 'tab:purple'
         elif self.zug.nummer < 2000:
             return 'tab:green'
         elif self.zug.nummer < 10000:
             return 'tab:blue'
         elif self.zug.nummer < 30000:
-            return 'tab:cyan'
+            return 'tab:purple'
         else:
             return 'tab:brown'
+
+    @property
+    def randfarbe(self) -> str:
+        if self.konflikte:
+            return 'r'
+        elif self.kuppelzug:
+            return 'g'
+        else:
+            return 'k'
 
     @property
     def titel(self) -> str:
@@ -113,65 +117,63 @@ class GleisbelegungWindow(QtWidgets.QMainWindow):
         self._balken = None
         self._labels = []
 
-        self._belegte_gruppen: List[str] = []
+        self._belegte_gleise: List[str] = []
         self._slots: List[Slot] = []
 
-        self.zeitfenster_voraus = 60
-        self.zeitfenster_zurueck = 0
+        self.zeitfenster_voraus = 55
+        self.zeitfenster_zurueck = 5
 
     def update(self):
-        if self._balken is not None:
-            self._balken.remove()
-        for label in self._labels:
-            label.remove()
+        self._axes.clear()
 
         kwargs = dict()
         kwargs['align'] = 'center'
         kwargs['alpha'] = 0.5
-        kwargs['edgecolor'] = 'black'
-        kwargs['linewidth'] = 1
         kwargs['width'] = 1.0
 
         self.belegung_berechnen()
-        x_labels = self._belegte_gruppen
+        x_labels = self._belegte_gleise
         x_labels_pos = list(range(len(x_labels)))
-        x_pos = np.asarray([self._belegte_gruppen.index(slot.gruppe) for slot in self._slots])
+        x_pos = np.asarray([self._belegte_gleise.index(slot.gleis) for slot in self._slots])
         y_bot = np.asarray([slot.zeit for slot in self._slots])
         y_hgt = np.asarray([slot.dauer for slot in self._slots])
         labels = [slot.titel for slot in self._slots]
         colors = [slot.farbe for slot in self._slots]
         style = [slot.style for slot in self._slots]
+        edgecolors = [slot.randfarbe for slot in self._slots]
+        linewidth = [w for w in map(lambda f: 1 if f == 'k' else 2, edgecolors)]
 
         self._axes.set_xticks(x_labels_pos, x_labels, rotation=45, horizontalalignment='right')
         self._axes.yaxis.set_major_formatter(hour_minutes_formatter)
         self._axes.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(1))
         self._axes.yaxis.set_major_locator(mpl.ticker.MultipleLocator(10))
         self._axes.yaxis.grid(True, which='major')
+        self._axes.xaxis.grid(True)
 
         zeit = time_to_minutes(self.client.calc_simzeit())
         self._axes.set_ylim(bottom=zeit + self.zeitfenster_voraus, top=zeit - self.zeitfenster_zurueck, auto=False)
 
-        self._balken = self._axes.bar(x_pos, y_hgt, bottom=y_bot, data=None, color=colors, **kwargs)
+        self._balken = self._axes.bar(x_pos, y_hgt, bottom=y_bot, data=None, color=colors, edgecolor=edgecolors,
+                                      linewidth=linewidth, **kwargs)
         self._labels = self._axes.bar_label(self._balken, labels=labels, label_type='center',
                                             fontsize='small', fontstretch='condensed')
+        self._axes.axhline(y=0)
 
         self._axes.figure.tight_layout()
         self._axes.figure.canvas.draw()
 
     def belegung_berechnen(self):
-        gruppen = set()
+        gleise = set()
         slots = list()
 
         for zug in self.client.zugliste.values():
             for planzeile in zug.fahrplan:
-                gruppenname = planzeile.gleis
-                # gruppenname = self.config.suche_gleisgruppe(planzeile.gleis, self.config.bahnsteigsgruppen)
-                if gruppenname:
-                    gruppen.add(gruppenname)
+                if planzeile.gleis:
+                    gleise.add(planzeile.gleis)
                 else:
                     continue
 
-                slot = Slot(zug, planzeile, gruppenname)
+                slot = Slot(zug, planzeile, planzeile.gleis)
                 slot.zeit = time_to_minutes(slot.plan.an) + zug.verspaetung
                 try:
                     slot.dauer = max(1, time_to_minutes(slot.plan.ab) - time_to_minutes(slot.plan.an))
@@ -180,25 +182,30 @@ class GleisbelegungWindow(QtWidgets.QMainWindow):
 
                 # ersatzzug anhängen
                 if ersatzzug := planzeile.ersatzzug:
-                    slot.partner = ersatzzug
                     slot.dauer = max(1, time_to_minutes(ersatzzug.fahrplan[0].an) + zug.verspaetung - slot.zeit)
                 elif kuppelzug := planzeile.kuppelzug:
-                    slot.partner = kuppelzug
+                    slot.kuppelzug = kuppelzug
                     slot.dauer = max(1, time_to_minutes(kuppelzug.fahrplan[0].an) + kuppelzug.verspaetung - slot.zeit)
                 elif fluegelzug := planzeile.fluegelzug:
-                    slot.partner = fluegelzug
+                    slot.kuppelzug = fluegelzug
 
                 if slot not in slots:
                     slots.append(slot)
 
-        gruppen = sorted(gruppen, key=gleisname_sortkey)
+        gleise = sorted(gleise, key=gleisname_sortkey)
 
         # konflikte erkennen
         for s1, s2 in itertools.permutations(slots, r=2):
-            if s1.zug.gleis == s2.zug.gleis and s1.zeit <= s2.zeit < s1.zeit + s1.dauer:
-                if (s1.partner is None or s1.partner != s2.zug) and (s2.partner is None or s2.partner != s1.zug):
+            if s1.dauer >= 5:
+                s1.dauer = max(5, s1.dauer - s1.zug.verspaetung)
+            if s1.gleis == s2.gleis and s1.zeit <= s2.zeit < s1.zeit + s1.dauer:
+                if s1.kuppelzug is not None and s1.kuppelzug == s2.zug:
+                    s2.kuppelzug = s1.zug
+                elif s2.kuppelzug is not None and s2.kuppelzug == s1.zug:
+                    s1.kuppelzug = s2.zug
+                else:
                     s1.konflikte.append(s2)
                     s2.konflikte.append(s1)
 
         self._slots = slots
-        self._belegte_gruppen = gruppen
+        self._belegte_gleise = gleise
