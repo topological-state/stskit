0f7f0597d4e6657fbafa65c6d68b1c37e864110e
matthias muntwiler <matthias@muntwiler.net>
2024-02-26 20:17:09 +0100

    Loktausch zwischen zwei Zügen testen (Test schlägt fehl)

--- tests/test_ereignisgraph.py
+++ tests/test_ereignisgraph.py
@@ -10,7 +10,7 @@ class TestEreignisPrognose(unittest.TestCase):
     """
     Prognose testen in einem Beispielgraph
     """
-    def setUp(self):
+    def szenario1(self):
         self.zielgraph = ZielGraph()
 
         nodes = [
@@ -185,6 +185,8 @@ class TestEreignisPrognose(unittest.TestCase):
         - Anzahl Kanten nach Typen
         """
 
+        self.szenario1()
+
         # nx.write_gml(self.ereignisgraph, "ereignisgraph.gml", stringizer=str)
         self.assertGreaterEqual(len(self.ereignisgraph.nodes), 16)
         self.assertGreaterEqual(len(self.ereignisgraph.edges), 15)
@@ -227,6 +229,7 @@ class TestEreignisPrognose(unittest.TestCase):
         self.assertDictEqual(types, expected_types, "Kantentypen")
 
     def test_zugpfad(self):
+        self.szenario1()
         act = [(n[0], self.ereignisgraph.nodes[n]['typ']) for n in self.ereignisgraph.zugpfad(11)]
         exp = [(11, 'Ab'), (11, 'An'), (11, 'An'), (11, 'E')]
         self.assertListEqual(act, exp, "Zug 11")
@@ -244,6 +247,7 @@ class TestEreignisPrognose(unittest.TestCase):
         """
         Prognose testen: keine Verspätungen
         """
+        self.szenario1()
         self.ereignisgraph.prognose()
         act = [self.ereignisgraph.nodes(data='t', default='?')[n] for n in self.ereignisgraph.zugpfad(11)]
         exp = [300, 322, 332, 332 + PlanungParams.mindestaufenthalt_ersatz]
@@ -291,10 +295,211 @@ class TestEreignisPrognose(unittest.TestCase):
             act = [self.ereignisgraph.nodes(data='t', default='?')[n] for n in self.ereignisgraph.zugpfad(14)]
             self.assertListEqual(act, exp, f"Zug 14, v = {v}")
 
+        self.szenario1()
         _test(5)
         _test(10)
         _test(20)
         _test(-5)
+
+    def szenario2(self):
+        """
+        Loktausch zwischen 2 Zuegen (Stw Jenbach)
+        """
+        self.zielgraph = ZielGraph()
+
+        nodes = [
+            ZielGraphNode(
+                fid=(1, 0, 1),
+                zid=1,
+                typ='E',
+                flags='',
+                status='',
+                p_an=340,
+                p_ab=340,
+                v_an=0,
+                v_ab=0
+            ), # 0
+            ZielGraphNode(
+                fid=(1, 0, "A"),
+                zid=1,
+                typ='H',
+                flags='F(3)',
+                mindestaufenthalt=PlanungParams.mindestaufenthalt_fluegelung,
+                status='',
+                p_an=350,
+                p_ab=374,
+                v_an=0,
+                v_ab=0
+            ), # 1
+            ZielGraphNode(
+                fid=(1, 0, 2),
+                zid=1,
+                typ='A',
+                flags='',
+                mindestaufenthalt=0,
+                status='',
+                p_an=384,
+                v_an=0,
+                v_ab=0
+            ), # 2
+
+            ZielGraphNode(
+                fid=(2, 0, 2),
+                zid=2,
+                typ='E',
+                flags='',
+                mindestaufenthalt=0,
+                status='',
+                p_an=357,
+                p_ab=357,
+                v_an=0,
+                v_ab=0
+            ), # 3
+            ZielGraphNode(
+                fid=(2, 0, "B"),
+                zid=2,
+                typ='H',
+                flags='F(4)',
+                mindestaufenthalt=PlanungParams.mindestaufenthalt_fluegelung,
+                status='',
+                p_an=367,
+                p_ab=400,
+                v_an=0,
+                v_ab=0
+            ), # 4
+            ZielGraphNode(
+                fid=(2, 0, 1),
+                zid=2,
+                typ='A',
+                flags='',
+                mindestaufenthalt=0,
+                status='',
+                p_an=410,
+                p_ab=410,
+                v_an=0,
+                v_ab=0
+            ), # 5
+
+            ZielGraphNode(
+                fid=(3, 0, "C"),
+                zid=3,
+                typ='H',
+                flags='',
+                mindestaufenthalt=PlanungParams.mindestaufenthalt_planhalt,
+                status='',
+                p_an=350,
+                p_ab=355,
+                v_an=0,
+                v_ab=0
+            ), # 6
+            ZielGraphNode(
+                fid=(3, 0, "B"),
+                zid=3,
+                typ='H',
+                flags='K(2)',
+                mindestaufenthalt=PlanungParams.mindestaufenthalt_kupplung,
+                status='',
+                p_an=361,
+                v_an=0,
+                v_ab=0
+            ), # 7
+
+            ZielGraphNode(
+                fid=(4, 0, "D"),
+                zid=4,
+                typ='H',
+                flags='',
+                mindestaufenthalt=PlanungParams.mindestaufenthalt_planhalt,
+                status='',
+                p_an=367,
+                p_ab=368,
+                v_an=0,
+                v_ab=0
+            ), # 8
+            ZielGraphNode(
+                fid=(4, 0, "A"),
+                zid=4,
+                typ='H',
+                flags='K(1)',
+                mindestaufenthalt=PlanungParams.mindestaufenthalt_kupplung,
+                status='',
+                p_an=372,
+                v_an=0,
+                v_ab=0
+            ) # 9
+        ]
+
+        edges = [
+            (0, 1, 'P'),
+            (1, 2, 'P'),
+            (1, 6, 'F'),
+            (6, 7, 'K'),
+            (3, 4, 'P'),
+            (4, 5, 'P'),
+            (4, 8, 'F'),
+            (8, 2, 'K')
+        ]
+
+        for node in nodes:
+            self.zielgraph.add_node(node.fid, **node)
+
+        for edge in edges:
+            self.zielgraph.add_edge(nodes[edge[0]].fid, nodes[edge[1]].fid, typ=edge[2])
+
+        self.ereignisgraph = EreignisGraph()
+        self.ereignisgraph.zielgraph_importieren(self.zielgraph)
+
+    def test_import_szenario2(self):
+        """
+        Import aus Zielgraph im Szenario 2 überprüfen.
+
+        Wir testen summarisch:
+        - Isomorphie mit Referenzgraph
+        - Anzahl Knoten nach Typen
+        - Anzahl Kanten nach Typen
+        """
+
+        self.szenario2()
+
+        # nx.write_gml(self.ereignisgraph, "ereignisgraph-szenario2.gml", stringizer=str)
+        self.assertGreaterEqual(len(self.ereignisgraph.nodes), 16)
+        self.assertGreaterEqual(len(self.ereignisgraph.edges), 16)
+
+        iso_edges = [
+            (11, 12, 'P'),
+            (12, 13, 'F'),
+            (13, 14, 'H'),
+            (14, 15, 'H'),
+            (15, 16, 'P'),
+            (21, 22, 'P'),
+            (22, 23, 'F'),
+            (23, 24, 'H'),
+            (24, 25, 'H'),
+            (25, 26, 'P'),
+            (13, 31, 'H'),
+            (31, 32, 'P'),
+            (32, 24, 'K'),
+            (23, 41, 'H'),
+            (41, 42, 'P'),
+            (42, 14, 'K')
+        ]
+        iso_graph = nx.DiGraph()
+        for edge in iso_edges:
+            iso_graph.add_edge(edge[0], edge[1], typ=edge[2])
+
+        self.assertTrue(nx.is_isomorphic(self.ereignisgraph, iso_graph), 'isomorphic graph')
+
+        types = {'Ab': 0, 'An': 0, 'E': 0, 'K': 0, 'F': 0}
+        expected_types = {'Ab': 6, 'An': 6, 'E': 0, 'K': 2, 'F': 2}
+        for node, typ in self.ereignisgraph.nodes(data='typ'):
+            types[typ] += 1
+        self.assertDictEqual(types, expected_types, "Knotentypen")
+
+        types = {'P': 0, 'H': 0, 'E': 0, 'K': 0, 'F': 0}
+        expected_types = {'P': 6, 'H': 6, 'E': 0, 'K': 2, 'F': 2}
+        for u, v, typ in self.ereignisgraph.edges(data='typ'):
+            types[typ] += 1
+        self.assertDictEqual(types, expected_types, "Kantentypen")
 
 
 if __name__ == '__main__':
