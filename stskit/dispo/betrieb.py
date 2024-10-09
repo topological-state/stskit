09cce7439c7acff22733d0892a095daeb8e70fb7
matthias muntwiler <matthias@muntwiler.net>
2024-10-09 18:37:28 +0200

    Fdl-Korrekturen im Betrieb über Journals

--- stskit/dispo/betrieb.py
+++ stskit/dispo/betrieb.py
@@ -19,6 +19,7 @@ import os
 from typing import Any, Dict, Iterable, List, Mapping, Optional, Sequence, Set, Tuple, Union
 
 from stskit.dispo.anlage import Anlage
+from stskit.model.journal import GraphJournal, JournalIDType
 from stskit.model.bahnhofgraph import BahnhofElement
 from stskit.model.ereignisgraph import EreignisGraph, EreignisGraphNode, EreignisGraphEdge, EreignisLabelType
 from stskit.model.zielgraph import ZielGraph, ZielGraphEdge, ZielGraphNode, ZielLabelType
@@ -32,7 +33,6 @@ logger.addHandler(logging.NullHandler())
 class Betrieb:
     def __init__(self):
         self.anlage: Anlage = Anlage()
-        self.abhaengigkeiten = EreignisGraph()
 
     def update(self, anlage: Anlage, config_path: os.PathLike):
         self.anlage = anlage
@@ -145,13 +145,17 @@ class Betrieb:
         """
 
         eg = self.anlage.ereignisgraph
+        egj = GraphJournal(target_graph=eg)
+
         edge = EreignisGraphEdge(typ="A", zid=subjekt.zid, dt_fdl=wartezeit or 0, quelle='fdl')
         if eg.has_node(objekt) and eg.has_node(subjekt):
             if not dry_run:
-                eg.add_edge(objekt, subjekt, **edge)
-                self.abhaengigkeiten.add_edge(objekt, subjekt, dt_fdl=edge.dt_fdl)
-                self.anlage.ereignisgraph.prognose()
-                print("Abhängigkeit gesetzt:", subjekt, objekt, edge)
+                egj.add_edge(objekt, subjekt, **edge)
+                egj.replay()
+                typ = "Abfahrt" if objekt.typ == "Ab" else "Ankunft"
+                jid = JournalIDType(typ, subjekt.zid, eg.nodes[subjekt].plan_bst)
+                self.anlage.fdl_korrekturen.add_journal(jid, egj)
+                print("Korrektur gesetzt:", jid)
             return objekt, subjekt
         else:
             return None
@@ -182,7 +186,8 @@ class Betrieb:
 
         n = ereignis
         eg = self.anlage.ereignisgraph
-        update_noetig = False
+        egj = GraphJournal(target_graph=eg)
+
         for pre in eg.predecessors(n):
             edge_data = eg.edges[(pre, n)]
             if edge_data.typ != kantentyp:
@@ -193,25 +198,26 @@ class Betrieb:
                     startwert = edge_data.dt_fdl
                 except (AttributeError, KeyError):
                     startwert = 0
-                edge_data.dt_fdl = startwert + wartezeit
+                dt = startwert + wartezeit
             else:
-                edge_data.dt_fdl = wartezeit
-            self.abhaengigkeiten.add_edge(pre, n, dt_fdl=edge_data.dt_fdl)
-            print("Wartezeit geändert:", ereignis, pre, edge_data)
-            update_noetig = True
+                dt = wartezeit
+            egj.change_edge(pre, n, dt_fdl=dt)
 
-        if update_noetig:
-            self.anlage.ereignisgraph.prognose()
+        egj.replay()
+        jid = JournalIDType("Wartezeit", ereignis.zid, eg.nodes[ereignis].plan_bst)
+        self.anlage.fdl_korrekturen.add_journal(jid, egj)
+        print("Wartezeit geändert:", jid)
 
     def abfahrt_zuruecksetzen(self,
                               wartend: Union[EreignisLabelType, EreignisGraphNode, ZielLabelType, ZielGraphNode],
                               abzuwarten: Optional[Union[EreignisLabelType, EreignisGraphNode, ZielLabelType, ZielGraphNode]] = None):
         """
-        Abfahrtsabhängigkeit zurücksetzen
+        Abfahrtskorrektur zurücksetzen
 
         subjekt: Wartende Abfahrt
         objekt: Abzuwartendes Ereignis (Label oder zugeordnete node data).
             Wenn None (default), werden alle eingehenden Abhängigkeiten des Abfahrtsereignisses gelöscht.
+            Im Moment nicht verwendet.
         """
 
         wartend = self._get_ereignis_label(wartend, {'Ab'})
@@ -219,38 +225,16 @@ class Betrieb:
             abzuwarten = self._get_ereignis_label(abzuwarten, {'An'})
 
         eg = self.anlage.ereignisgraph
-        update_noetig = False
 
-        if abzuwarten is None:
-            preds = eg.predecessors(wartend)
-        else:
-            preds = [abzuwarten]
+        ereignis_data = eg.nodes[wartend]
+        for jid, j in self.anlage.fdl_korrekturen.collection.items():
+            if (jid.typ in {"Ankunft", "Abfahrt"} and
+                    jid.zid == wartend.zid and
+                    jid.bst == ereignis_data.plan_bst):
+                self.anlage.fdl_korrekturen.delete_journal(jid)
+                print("Korrektur gelöscht:", jid)
+                break
 
-        loeschen = []
-        for pre in preds:
-            try:
-                edge_data = eg.edges[(pre, wartend)]
-            except KeyError:
-                continue
-            if edge_data.typ != 'A':
-                continue
-
-            loeschen.append((pre, wartend))
-            update_noetig = True
-            print("Abhängigkeit gelöscht:", wartend, pre, edge_data)
-
-        for edge in loeschen:
-            try:
-                eg.remove_edge(*edge)
-            except KeyError:
-                pass
-            try:
-                self.abhaengigkeiten.remove_edge(*edge)
-            except KeyError:
-                pass
-
-        if update_noetig:
-            self.anlage.ereignisgraph.prognose()
 
     def _betriebshalt_statt_durchfahrt(self,
                                        durchfahrt: EreignisLabelType,
@@ -268,6 +252,7 @@ class Betrieb:
         """
 
         eg = self.anlage.ereignisgraph
+        zg = self.anlage.zielgraph
 
         an2_label = durchfahrt
         an3_label = eg.next_ereignis(an2_label)
@@ -285,21 +270,35 @@ class Betrieb:
 
         ab2_node = copy.copy(an2_node)
         ab2_node.typ = 'Ab'
+        ab2_node.quelle = 'fdl'
         ab2_label = ab2_node.node_id
 
         halt_edge = copy.copy(edge_alt)
         halt_edge.typ = 'B'
+        halt_edge.quelle = 'fdl'
         halt_edge.dt_min = 0
         halt_edge.dt_max = math.inf
         halt_edge.dt_fdl = wartezeit
         halt_edge.ds = 0
 
         abfahrt_edge = copy.copy(edge_alt)
+        abfahrt_edge.quelle = 'fdl'
 
-        eg.add_node(ab2_label, **ab2_node)
-        eg.add_edge(an2_label, ab2_label, **halt_edge)
-        eg.add_edge(ab2_label, an3_label, **abfahrt_edge)
-        eg.remove_edge(an2_label, an3_label)
+        egj = GraphJournal(target_graph=eg)
+        egj.add_node(ab2_label, **ab2_node)
+        egj.add_edge(an2_label, ab2_label, **halt_edge)
+        egj.add_edge(ab2_label, an3_label, **abfahrt_edge)
+        egj.remove_edge(an2_label, an3_label)
+
+        zgj = GraphJournal(target_graph=zg)
+        ziel = zg.nodes[an2_node.fid]
+        zgj.change_node(an2_node.fid, flags=ziel.flags.replace('D', ''))
+
+        egj.replay()
+        zgj.replay()
+
+        jid = JournalIDType(typ="Betriebshalt", zid=an2_node.zid, bst=an2_node.plan_bst)
+        self.anlage.fdl_korrekturen.add_journal(jid, egj, zgj)
 
     def _betriebshalt_auf_strecke(self,
                                   vorher: EreignisLabelType,
@@ -313,9 +312,17 @@ class Betrieb:
         durch `Ab1 --P--> An2 --B--> Ab2 --P--> An3`.
         Es werden zwei neue Knoten und drei neue Kanten eingefuegt.
         Die Kante von Ab1 nach An3 wird entfernt.
+
+        Argumente
+        ---------
+
+        vorher: Vorhergehendes Ereignis im Ereignisgraph
+
+        bst: Plangleis in Betriebsstellen-Notation. Muss vom Typ 'Gl' sein.
         """
 
         eg = self.anlage.ereignisgraph
+        zg = self.anlage.zielgraph
 
         ab1_label = vorher
         an3_label = eg.next_ereignis(ab1_label)
@@ -330,6 +337,8 @@ class Betrieb:
             raise ValueError(f"Ursprungsereignis {ab1_label} ist keine Abfahrt.")
         if an3_node.typ != 'An':
             raise ValueError(f"Folgeereignis {ab1_label} ist keine Ankunft.")
+        if bst.typ != 'Gl':
+            raise ValueError(f"Ungültige Gleisbezeichnung {bst}.")
 
         teiler: float = 0.5
 
@@ -338,10 +347,10 @@ class Betrieb:
         an2_node.typ = 'An'
         an2_node.fid = None
         an2_node.plan = an2_node.gleis = bst.name
+        an2_node.plan_bst = an2_node.gleis_bst = bst
         an2_node.t_plan = an2_node.zeit = ab1_node.zeit + teiler * (an3_node.zeit - ab1_node.zeit)
         an2_node.t_mess = None
         an2_node.s = an2_node.zeit = ab1_node.s + teiler * (an3_node.s - ab1_node.s)
-        an2_node.bst = self.anlage.bahnhofgraph.find_superior(bst, {"Bft"})
         an2_label = an2_node.node_id
 
         ab2_node = copy.copy(an2_node)
@@ -370,75 +379,34 @@ class Betrieb:
         abfahrt_edge.dt_max = math.inf
         abfahrt_edge.dt_fdl = 0
 
-        eg.add_node(ab2_label, **ab2_node)
-        eg.add_node(an2_label, **an2_node)
-        eg.add_edge(ab1_label, an2_label, **ankunft_edge)
-        eg.add_edge(an2_label, ab2_label, **halt_edge)
-        eg.add_edge(ab2_label, an3_label, **abfahrt_edge)
-        eg.remove_edge(ab1_label, an3_label)
+        egj = GraphJournal(target_graph=eg)
+        egj.add_node(ab2_label, **ab2_node)
+        egj.add_node(an2_label, **an2_node)
+        egj.add_edge(ab1_label, an2_label, **ankunft_edge)
+        egj.add_edge(an2_label, ab2_label, **halt_edge)
+        egj.add_edge(ab2_label, an3_label, **abfahrt_edge)
+        egj.remove_edge(ab1_label, an3_label)
+        egj.replay(eg)
+
+        jid = JournalIDType(typ="Betriebshalt", zid=an2_node.zid, bst=an2_node.plan_bst)
+        self.anlage.fdl_korrekturen.add_journal(jid, egj)
 
     def _betriebshalt_loeschen(self, halt_ereignis: EreignisLabelType):
         """
         Betriebshalt aus Ereignisgraph loeschen
 
-        Ersetzt die Ereignisfolge `Ab1 --P-> An2 --B-> Ab2 --P-> An3`
-        durch `Ab1 --P--> An2 --P--> An3`, wenn An2 im Fahrplan ist,
-        sonst `Ab1 --P--> An3`.
+        Der Betriebshalt wird aus dem fdl_korrekturen-Journal gelöscht.
+        Der Ereignisgraph wird direkt nicht verändert und muss neu aufgebaut werden.
 
         halt_ereignis: Ereignislabel von An2 oder Ab2
         """
-        eg = self.anlage.ereignisgraph
 
-        halt_node = eg.nodes[halt_ereignis]
-        if halt_node.typ == 'An':
-            an2_label = halt_ereignis
-            ab2_label = eg.next_ereignis(halt_ereignis)
-        elif halt_node.typ == 'Ab':
-            ab2_label = halt_ereignis
-            an2_label = eg.prev_ereignis(halt_ereignis)
-        else:
-            raise ValueError(f"{halt_ereignis} ist kein Betriebshalt.")
-
-        halt_edge = eg.edges[(an2_label, ab2_label)]
-
-        an2_node = eg.nodes[an2_label]
-        ab2_node = eg.nodes[ab2_label]
-
-        ab1_label = eg.prev_ereignis(an2_label)
-        an3_label = eg.next_ereignis(ab2_label)
-        ab1_node = eg.nodes[ab1_label]
-        an3_node = eg.nodes[an3_label]
-
-        ankunft_edge = eg.edges[(ab1_label, an2_label)]
-        abfahrt_edge = eg.edges[(ab2_label, an3_label)]
-
-        if ankunft_edge.typ != 'P':
-            raise ValueError(f"Abfolge {ab1_label}-{an2_label} ist keine Planfahrt.")
-        if abfahrt_edge.typ != 'P':
-            raise ValueError(f"Abfolge {ab2_label}-{an3_label} ist keine Planfahrt.")
-        if ab1_node.typ != 'Ab':
-            raise ValueError(f"Ursprungsereignis {ab1_label} ist keine Abfahrt.")
-        if an3_node.typ != 'An':
-            raise ValueError(f"Folgeereignis {ab1_label} ist keine Ankunft.")
-        if halt_edge.typ != 'B':
-            raise ValueError(f"Abfolge {an2_label}-{ab2_label} ist kein Betriebshalt.")
-
-        if an2_node.fid:
-            # An2 ist im Fahrplan
-            edge_neu = copy.copy(abfahrt_edge)
-            eg.remove_node(ab2_label)
-            eg.add_edge(an2_label, an3_label, **edge_neu)
-        else:
-            # An2 ist nicht im Fahrplan
-            edge_neu = copy.copy(ankunft_edge)
-            edge_neu.dt_min = ankunft_edge.dt_min + abfahrt_edge.dt_min
-            edge_neu.ds = ankunft_edge.ds + abfahrt_edge.ds
-            edge_neu.dt_max = math.inf
-            edge_neu.dt_fdl = 0
-
-            eg.remove_node(an2_label)
-            eg.remove_node(ab2_label)
-            eg.add_edge(ab1_label, an3_label, **edge_neu)
+        ereignis_label = self._get_ereignis_label(halt_ereignis, {'An'})
+        ereignis_data = self.anlage.ereignisgraph.nodes[ereignis_label]
+        for jid, j in self.anlage.fdl_korrekturen.collection.items():
+            if jid.typ == "Betriebshalt" and jid.zid == halt_ereignis.zid and jid.bst == ereignis_data.plan_bst:
+                self.anlage.fdl_korrekturen.delete_journal(jid)
+                break
 
     def betriebshalt_einfuegen(self,
                                vorheriges_ziel: Union[EreignisLabelType, EreignisGraphNode, ZielLabelType, ZielGraphNode],
