6194ba125f28e29567701e85d22791a882d38fb8
matthias muntwiler <matthias@muntwiler.net>
2025-06-14 17:43:09 +0200

    compilation with nuitka

--- stsdispo.py
+++ stsdispo.py
@@ -1,3 +1,16 @@
+# nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --static-libpython=no
+# nuitka-project: --include-data-files=stskit/mplstyle/*.mplstyle=stskit/mplstyle/
+# nuitka-project: --include-data-files=stskit/qt/*.css=stskit/qt/
+# nuitka-project: --include-data-files=stskit/qt/*.ui=stskit/qt/
+# nuitka-project: --include-data-files=stskit/config/*.json=stskit/config/
+
+# to compile with nuitka:
+# uv run python -m nuitka --standalone stsdispo.py
+# or:
+# uv run python -m nuitka --onefile stsdispo.py
+
+
 from stskit.__main__ import main
 
 if __name__ == '__main__':
