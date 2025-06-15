dc5a97386e3b4ed37892d3a273b60b1e097183ef
matthias muntwiler <matthias@muntwiler.net>
2025-06-15 15:54:16 +0200

    tweak nuitka options

--- stsdispo.py
+++ stsdispo.py
@@ -5,6 +5,15 @@
 # nuitka-project: --include-data-files=stskit/qt/*.ui=stskit/qt/
 # nuitka-project: --include-data-files=stskit/config/*.json=stskit/config/
 
+# Compilation mode, standalone everywhere, except on macOS there app bundle
+# nuitka-project: --mode=app
+#
+# Debugging options, controlled via environment variable at compile time.
+# nuitka-project-if: {OS} == "Windows" and os.getenv("DEBUG_COMPILATION", "no") == "yes"
+#     nuitka-project: --windows-console-mode=hide
+# nuitka-project-else:
+#     nuitka-project: --windows-console-mode=disabled
+
 # to compile with nuitka:
 # uv run python -m nuitka --standalone stsdispo.py
 # or:
